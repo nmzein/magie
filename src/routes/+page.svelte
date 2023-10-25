633b448a982608ee88980651108ceeff5434a1da
Noureldin Zein <noureldinzein@gmail.com>
2023-10-25 01:37:32 +0100

    fix: moved global css to separate file

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,8 @@
 <script lang="ts">
-    import View from "$lib/components/View.svelte";
-    import Controls from "$lib/components/Controls.svelte";
+	import '../global.scss';
+	import View from '$lib/components/View.svelte';
+	import Controls from '$lib/components/Controls.svelte';
 </script>
 
 <View />
 <Controls />
-
-<style>
-    :global(body) {
-        margin: 0;
-        background-color: green;
-    }
-
-    :global(button) {
-        border: none;
-        cursor: pointer;
-    }
-</style>
