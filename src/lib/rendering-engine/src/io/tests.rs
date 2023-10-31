cb6195f1ce56ab20f932d57258bf3efa3ca0a0c9
Noureldin Zein <noureldinzein@gmail.com>
2023-10-31 15:17:06 +0000

    feat: tests for image and io

---
+++ src/lib/rendering-engine/src/io/tests.rs
@@ -0,0 +1,36 @@
+use crate::io;
+
+use std::{fs, path::PathBuf};
+use anyhow::Result;
+
+#[cfg(feature = "immediately_write_to_hdf5")]
+#[test]
+fn test_io_create_hdf5() -> Result<()>{
+    let num_tiles = 1500;
+    let tile_size: usize = 1024;
+    let max_tile_length: usize = 110_000;
+
+    let h5_path = PathBuf::from("test_temp/test_io_create_h5.h5");
+
+    // Cleanup from previous test in case it failed.
+    let _ = fs::remove_file(&h5_path);
+
+    // Get the dataset.
+    let dataset = io::create_hdf5(&h5_path, num_tiles)?;
+    
+    // Verify that the dataset is 2D dataset.
+    let dataspace = dataset.space()?;
+    assert_eq!(dataspace.ndim(), 2);
+
+    // Get the dimensions.
+    let dimensions = dataspace.shape();
+
+    // Check if the dimensions match the expected values.
+    let expected_dimensions = [num_tiles, max_tile_length];
+    assert_eq!(dimensions, expected_dimensions);
+
+    // Cleanup.
+    let _ = fs::remove_file(&h5_path);
+
+    Ok(())
+}
