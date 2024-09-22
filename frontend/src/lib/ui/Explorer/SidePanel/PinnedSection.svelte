0069067a510ed194639ddcada9166786057d2df5
nmzein <67694622+nmzein@users.noreply.github.com>
2024-09-22 15:32:17 +0300

    fix: pinned section for images

--- frontend/src/lib/ui/Explorer/SidePanel/PinnedSection.svelte
+++ frontend/src/lib/ui/Explorer/SidePanel/PinnedSection.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { explorer } from '$states';
+	import { explorer, image } from '$states';
 	import Button from '$components/Button.svelte';
 	import Section from './Section.svelte';
 </script>
@@ -7,7 +7,13 @@
 <Section title="PINNED">
 	{#each explorer.pinned as item}
 		<Button
-			onclick={() => explorer.routeTo(item.route)}
+			onclick={() => {
+				if (item.data.type === 'directory') {
+					explorer.routeTo(item.route);
+				} else {
+					image.load(item.data);
+				}
+			}}
 			class="hover:bg-primary/15 flex w-full items-center gap-[10px] rounded-lg px-[10px] py-[7.5px] text-left"
 		>
 			{item.data.name}
