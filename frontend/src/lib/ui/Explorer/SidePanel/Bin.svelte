5f893179ddb61cc51c5b503aa4342449bf302bad
nmzein <67694622+nmzein@users.noreply.github.com>
2024-09-22 10:58:33 +0300

    fix: move performance issues

--- frontend/src/lib/ui/Explorer/SidePanel/Bin.svelte
+++ frontend/src/lib/ui/Explorer/SidePanel/Bin.svelte
@@ -7,7 +7,7 @@
 <!-- Directory with id of 1 is always set to the global bin. -->
 <Button
 	onclick={() => explorer.routeTo([1])}
-	class="bg-primary/10 hover:bg-primary/15 flex w-full flex-row items-center gap-[10px] rounded-lg px-[10px] py-[7.5px]"
+	class="bg-primary/10 hover:bg-primary/15 flex w-full select-none flex-row items-center gap-[10px] rounded-lg px-[10px] py-[7.5px]"
 >
 	<Icon name="trash" class="h-6 w-6" />
 	Bin
